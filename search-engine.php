21377a92ce105ef4d68d655be160f2490431b7c0
roma-melo <romario.melo.barreiras@gmail.com>
2021-01-26 18:35:32 -0300

    Create bin file

---
+++ search-engine.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+
+require('vendor/autoload.php');
+
+use App\Service\SearchEngine;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client  = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$searchEngine = (new SearchEngine($client, $crawler))
+    ->setMethod('GET')
+    ->setSelector('span.card-curso__nome')
+    ->setUrl('https://www.alura.com.br/cursos-online-programacao/php')
+    ->run();
+
+foreach ($searchEngine as $value) {
+    echo $value;
+}
